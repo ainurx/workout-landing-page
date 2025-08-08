db473790c218ce8a0558cadd96e656497c27b1c9
ainurx <ainurrahmat1@gmail.com>
2025-08-08 10:16:15 +0800

    [master] card linear adj

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -15,10 +15,9 @@ const Card: React.FC<itemInterface> = ({item}) => {
     const { title, image, duration, cal } = item
   return (
     <div className='w-full relative'>
-        <Image src={image} className='w-full z-0' alt={title}/>
-        <div className='h-10 z-30 bg-linear-to-t from-black to-zinc-900 to-75%'/>
-        <div className='px-6 z-20'>
-            <div className='-mt-7 flex justify-between'>
+        <Image src={image} className='w-full rounded-t-xl mask-b-from-30% mask-b-to-100%' alt={title}/>
+        <div className='-mt-7 px-6'>
+            <div className='flex justify-between'>
                 <h2 className='text-5xl font-semibold'>{title}</h2>
                 <p className='bg-black p-2 right-0 mt-2 rounded-tl-2xl absolute'>{duration}</p>
             </div>
